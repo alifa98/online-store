1ed2e2f99038e3fb4f58e7e1e163caf5171314d3
Soroush Mehraban <Smehraban2013@gmail.com>
2021-07-18 14:28:58 +0430

    Backend is added, database is created

---
+++ online-store/backend/main/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, null=False, default="دسته بندی نشده", unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+def default_category():
+    return Category.objects.get(name='دسته بندی نشده').pk
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default=default_category())
+    price = models.FloatField(default=0)
+    available_amount = models.IntegerField(default=0)
+    sold_amount = models.IntegerField(default=0)
+    image = models.ImageField(default='product.png', upload_to="uploaded_product/", blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+receipt_state = [
+  ('در حال انجام', 'در حال انجام'),
+  ('انجام شده', 'انجام شده'),
+  ('لفو شده', 'لغو شده'),
+]
+
+
+class Receipt(models.Model):
+    product_name = models.CharField(max_length=200)
+    sold_amount = models.IntegerField(default=0)
+    buyer_full_name = models.CharField(max_length=300)
+    buyer_address = models.CharField(max_length=1000)
+    price = models.FloatField(default=0)
+    purchase_date = models.DateTimeField(default=timezone.now)
+    state = models.CharField(max_length=100, choices=receipt_state, default='در حال انجام')
+
+    def __str__(self):
+        return f"{self.product_name}: {self.buyer_full_name}"
