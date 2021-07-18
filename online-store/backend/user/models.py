1bd449f35985f7a852d27e38c2178916c7ef2bba
Soroush Mehraban <Smehraban2013@gmail.com>
2021-07-18 18:02:54 +0430

    login view is added to backed

--- online-store/backend/user/models.py
+++ online-store/backend/user/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     address = models.CharField(max_length=1000)
     credit = models.FloatField(default=0)
--- online-store/backend/user/views.py
+++ online-store/backend/user/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth import authenticate, login
+from django.core import serializers
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({"success": True})
+        else:
+            return JsonResponse({"success": False})
+
+    if request.method == "GET":
+        return JsonResponse(serializers.serialize('json', request.user))
